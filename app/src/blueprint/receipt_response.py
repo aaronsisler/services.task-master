67a4a9f86060c9271bb9829c5dda96a968352533
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2025-04-04 16:36:29 -0400

    Trying to see if I can check stack existence

--- app/src/blueprint/receipt_response.py
+++ app/src/blueprint/receipt_response.py
@@ -1,7 +1,8 @@
 class ReceiptResponse:
-    def __init__(self, stack_id):
+    def __init__(self, stack_id, does_stack_exist):
         super().__init__()
         self.stack_id = stack_id
+        self.does_stack_exist = does_stack_exist
 
     def default(self, o):
         return o.__dict__
--- app/src/util/cloudformation_stack_util.py
+++ app/src/util/cloudformation_stack_util.py
@@ -1,3 +1,6 @@
+import boto3
+from botocore.exceptions import ClientError
+
 from app.src.blueprint.receipt_response import ReceiptResponse
 
 
@@ -5,5 +8,18 @@ def create():
     return True
 
 
-def delete():
-    return ReceiptResponse(stack_id="this is a stack id")
+def delete(stack_name):
+    does_stack_exist = stack_exists(stack_name)
+    print("does_stack_exist")
+    print(does_stack_exist)
+    return ReceiptResponse(stack_id=stack_name, does_stack_exist=does_stack_exist)
+
+
+def stack_exists(name, required_status='CREATE_COMPLETE'):
+    try:
+        client = boto3.client('cloudformation')
+        data = client.describe_stacks(StackName=name)
+    except ClientError:
+        return False
+
+    return data['Stacks'][0]['StackStatus'] == required_status
