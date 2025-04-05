72613e1f3282d75b9cbff0966de801ecc92ae286
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2025-04-05 06:12:59 -0400

    Tweaks for reading file content instead of the file as input

--- app/src/util/cloudformation_template_util.py
+++ app/src/util/cloudformation_template_util.py
@@ -1,12 +1,8 @@
-import yaml
+import json
 
 
 def get_ecs_template_content():
     file_path = "app/src/template/ecs_template.yaml"
 
-    with open(file_path) as stream:
-        try:
-            return yaml.safe_load(stream)
-        except yaml.YAMLError as exception:
-            print(exception)
-            raise Exception("Error reading template file")
+    with open(file_path) as f:
+        return json.load(f)
