5a1f899b208404861aca7531605a93bdfd9b8847
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2025-04-21 21:17:58 -0400

    Adding in the logic for a shortname given 32 char limit on TG

--- app/src/blueprint/create_stack_request.py
+++ app/src/blueprint/create_stack_request.py
@@ -1,6 +1,7 @@
 class CreateStackRequest:
-    def __init__(self, service_name, dns_prefix, cost_center_tag, domain_name="otterandcow.com"):
+    def __init__(self, service_name, service_short_name, dns_prefix, cost_center_tag, domain_name="otterandcow.com"):
         self.service_name = service_name
+        self.service_short_name = service_short_name
         self.dns_prefix = dns_prefix
         self.cost_center_tag = cost_center_tag
         self.domain_name = domain_name
